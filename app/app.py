f8db9df959bf3f55f43e663f97201bae9edf65dc
Colm Kelleher <ckcolmkelleher@gmail.com>
2024-09-22 19:28:38 +0100

    Moved app.py to correct folder

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from app.config import Config
-from app.models import Lead, Interaction, Task
+from config import Config
+from models import Lead, Interaction, Task
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
+@app.route('/')
+def home():
+    return "Home Page!!"
 
 if __name__ == '__main__':
     app.run(debug=True)
