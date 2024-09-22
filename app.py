dba1bc0dd4908ab1389e90fd8f7586af3f2f5410
Colm Kelleher <ckcolmkelleher@gmail.com>
2024-09-22 19:17:40 +0100

    update app.py calls

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
-from models import Lead, Interaction, Task
+from app.config import Config
+from app.models import Lead, Interaction, Task
 
 app = Flask(__name__)
 app.config.from_object(Config)
