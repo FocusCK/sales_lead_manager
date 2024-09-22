c4c39e5708663eba230cef04c3c86b7feaf1f571
Colm Kelleher <ckcolmkelleher@gmail.com>
2024-09-22 16:37:01 +0100

    add interaction class

--- app/models.py
+++ app/models.py
@@ -5,3 +5,12 @@ class Lead(db.Model):
     status = db.Column(db.String(50), default="New")
     interactions = db.relationship('Interaction', backref='lead', lazy=True)
     tasks = db.relationship('Task', backref='lead', lazy=True)
+
+
+
+class Interaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lead_id = db.Column(db.Integer, db.ForeignKey('lead.id'), nullable=False)
+    date = db.Column(db.DateTime, default=db.func.now())
+    interaction_type = db.Column(db.String(50))
+    notes = db.Column(db.Text)
